12dc9741b844a1b17a276a0a3004cbfdad483c85
jsc <jsc1989@gmail.com>
2023-01-14 04:47:39 +0800

    1st commit: Task 7: Use the Correct Data Type

---
+++ src/com/group7/WorkHours.java
@@ -0,0 +1,46 @@
+package com.group7;
+
+
+public class WorkHours {
+
+    int minutes;
+    int hours;
+    public WorkHours(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+    public static void main(String[] args) {
+
+        //create Time class objects
+        WorkHours start = new WorkHours(8, 0);
+        WorkHours stop = new WorkHours(17, 0);
+        WorkHours diff;
+
+        //call difference method
+        diff = difference(start, stop);
+
+        //print hours worked with correct formatting hh:mm
+        System.out.printf("Hours Worked: %d:%02d\n", diff.hours, diff.minutes);
+
+    }
+
+    public static WorkHours difference(WorkHours start, WorkHours stop) {
+
+        WorkHours diff = new WorkHours(0,0);
+
+        //if the start minute is greater
+        //decrement hour
+        //convert one stop hour to minutes
+        //then add the converted minutes to stop minutes
+        if(start.minutes > stop.minutes){
+            --stop.hours;
+            stop.minutes += 60;
+        }
+
+        diff.minutes = stop.minutes - start.minutes;
+        diff.hours = stop.hours - start.hours;
+
+        //return the difference time
+        return(diff);
+    }
+}
